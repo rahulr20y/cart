8db7b00c921c9ccc46b1f061032e4bc82c5c1317
Rahul Yadav <rahulr20y@gmail.com>
2021-05-08 17:21:58 +0530

    Moving state from Cart to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,92 @@
 
+import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Cart />
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(){
+      super();
+      this.state = {
+          products: [
+              {
+                  price : 9999,
+                  title : 'Phone',
+                  qty : 1,
+                  img : "",
+                  id : 1
+              },
+              {
+                  price : 999,
+                  title : 'Watch',
+                  qty : 2,
+                  img : "",
+                  id: 2
+              },
+              {
+                  price : 50000,
+                  title : 'Laptop',
+                  qty : 1,
+                  img : "",
+                  id : 3
+              }
+          ]
+      }
+      //this.increaseQuantity = this.increaseQuantity.bind(this);
+      //this.testing();
+  }
+
+
+  handleIncreaseQuantity = (product) => {
+      let {products} = this.state;
+      let index = products.indexOf(product);
+
+      products[index].qty = product.qty + 1;
+
+      this.setState({
+          products : products
+      });
+  }
+  handleDecreaseQuantity = (product) => {
+      let {products} = this.state;
+      let index = products.indexOf(product);
+
+      if(products[index].qty > 0){
+          products[index].qty = product.qty - 1;
+      }
+
+      this.setState({
+          products : products
+      });
+  }
+  handleDeleteProduct = (id) => {
+      let {products} = this.state;
+      let items = products.filter((item) => {
+          return item.id !== id
+      });
+
+      this.setState({
+          products : items
+      });
+  }
+
+
+  render() {
+       
+      const {products} = this.state;
+      
+      return (
+        <div className="App">
+          <Navbar />
+          <Cart 
+            products={products}
+            handleIncreaseQuantity={this.handleIncreaseQuantity} 
+            handleDecreaseQuantity={this.handleDecreaseQuantity}
+            handleDeleteProduct={this.handleDeleteProduct} 
+          />
+        </div>
+      );
+  }
 }
 
 export default App;
--- src/Cart.js
+++ src/Cart.js
@@ -1,99 +1,26 @@
 import React from 'react';
 import CartItem from './CartItem';
 
-class Cart extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            products: [
-                {
-                    price : 9999,
-                    title : 'Phone',
-                    qty : 1,
-                    img : "",
-                    id : 1
-                },
-                {
-                    price : 999,
-                    title : 'Watch',
-                    qty : 2,
-                    img : "",
-                    id: 2
-                },
-                {
-                    price : 50000,
-                    title : 'Laptop',
-                    qty : 1,
-                    img : "",
-                    id : 3
-                }
-            ]
-        }
-        //this.increaseQuantity = this.increaseQuantity.bind(this);
-        //this.testing();
-    }
+const Cart = (props) => {
+    
+    const { products } = props;
+    return (
+        <div className="cart">
+            { products.map( (product) => {
+                return (
+                    <CartItem
+                        product={product}
+                        key={product.id}
+                        handleIncreaseQuantity={props.handleIncreaseQuantity} 
+                        handleDecreaseQuantity={props.handleDecreaseQuantity}
+                        handleDeleteProduct={props.handleDeleteProduct}  
+                    />
+                );
 
-
-    handleIncreaseQuantity = (product) => {
-        let {products} = this.state;
-        let index = products.indexOf(product);
-
-        products[index].qty = product.qty + 1;
-
-        this.setState({
-            products : products
-        });
-    }
-    handleDecreaseQuantity = (product) => {
-        let {products} = this.state;
-        let index = products.indexOf(product);
-
-        if(products[index].qty > 0){
-            products[index].qty = product.qty - 1;
-        }
-
-        this.setState({
-            products : products
-        });
-    }
-    handleDeleteProduct = (id) => {
-        let {products} = this.state;
-        let items = products.filter((item) => {
-            return item.id !== id
-        });
-
-        this.setState({
-            products : items
-        });
-    }
-
-
-    render(){
-        // const arr = [1, 2, 3, 4, 5];
-        const { products } = this.state;
-        return (
-            <div className="cart">
-                {/* {arr.map((item)=>{
-                    return item+5;
-                })} */}
-
-                {/* <CartItem qty={1} price={99} title={"Watch"} img={''}/> */}
-
-                { products.map( (product) => {
-                    return (
-                        <CartItem
-                           product={product}
-                           key={product.id}
-                           handleIncreaseQuantity={this.handleIncreaseQuantity} 
-                           handleDecreaseQuantity={this.handleDecreaseQuantity}
-                           handleDeleteProduct={this.handleDeleteProduct}  
-                        />
-                    );
-
-                } ) }
-            </div>
-        )
-    }
+            } ) }
+        </div>
+    )
+    
 }
 
 export default Cart;
